643885e09ccb9175c378f9ac82409b8882db0b74
yasuotoko <yuchanan@icloud.com>
2019-06-06 00:21:22 +0800

    maven spring mvc tomcat

---
+++ src/main/java/com/zbf/config/RootConfig.java
@@ -0,0 +1,10 @@
+package com.zbf.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages={"com.zbf.service"})
+public class RootConfig {
+
+}
